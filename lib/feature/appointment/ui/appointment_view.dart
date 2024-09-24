189e2906835fdfa6ceecf5a191a904477044b57a
salma <salmaelmaghawry91@gmail.com>
2024-09-24 11:41:04 +0300

    appointment

--- lib/feature/appointment/ui/appointment_view.dart
+++ lib/feature/appointment/ui/appointment_view.dart
@@ -1,28 +1,32 @@
 import 'package:booking_clinics/core/common/custom_button.dart';
+import 'package:booking_clinics/core/constant/const_color.dart';
+import 'package:booking_clinics/data/models/booking.dart';
+import 'package:booking_clinics/data/services/remote/firebase_auth.dart';
+import 'package:booking_clinics/feature/booking/ui/view/book_appointment.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../../../core/constant/const_color.dart';
-import '../../../data/services/remote/firebase_auth.dart';
-import 'widgets/booking_appbar.dart';
-import 'widgets/build_booking_card.dart';
 
 class BookingService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final String _patientsCollection = 'patients';
 
+  // Fetch bookings by status
   Future<List<Map<String, dynamic>>> fetchBookings(String status) async {
     try {
       String? patientId = await FirebaseAuthService().getUid();
 
-      final patientRef = _firestore.collection(_patientsCollection).doc(patientId);
+      final patientRef =
+          _firestore.collection(_patientsCollection).doc(patientId);
       final docSnapshot = await patientRef.get();
 
       if (docSnapshot.exists) {
         final data = docSnapshot.data();
         if (data != null && data['bookings'] != null) {
-          final bookingsJson = List<Map<String, dynamic>>.from(data['bookings']);
-          // Filter bookings by status
-          return bookingsJson.where((booking) => booking['bookingStatus'] == status).toList();
+          final bookingsJson =
+              List<Map<String, dynamic>>.from(data['bookings']);
+          return bookingsJson
+              .where((booking) => booking['bookingStatus'] == status)
+              .toList();
         }
       }
       return [];
@@ -31,99 +35,66 @@ class BookingService {
       throw Exception('Failed to fetch bookings');
     }
   }
+
+  Future<void> updateBookingStatus(String bookingId, String newStatus) async {
+    try {
+      String? patientId = await FirebaseAuthService().getUid();
+
+      final patientRef =
+          _firestore.collection(_patientsCollection).doc(patientId);
+      final docSnapshot = await patientRef.get();
+
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data();
+        if (data != null && data['bookings'] != null) {
+          final bookings = List<Map<String, dynamic>>.from(data['bookings']);
+          final bookingIndex =
+              bookings.indexWhere((booking) => booking['id'] == bookingId);
+
+          if (bookingIndex != -1) {
+            bookings[bookingIndex]['bookingStatus'] = newStatus;
+
+            await patientRef.update({
+              'bookings': bookings,
+            });
+          }
+        }
+      }
+    } catch (e) {
+      print('Error updating booking status: $e');
+      throw Exception('Failed to update booking status');
+    }
+  }
 }
 
 class AppointmentView extends StatefulWidget {
-  const AppointmentView({super.key});
-
   @override
-  State<AppointmentView> createState() => _AppointmentViewState();
+  _AppointmentViewState createState() => _AppointmentViewState();
 }
 
-class _AppointmentViewState extends State<AppointmentView>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+class _AppointmentViewState extends State<AppointmentView> {
   final BookingService _bookingService = BookingService();
+  
 
   @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BookingAppBar(tabController: _tabController),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          buildBookingsTab(context, 'Pending'),
-          buildBookingsTab(context, 'Completed'),
-          buildBookingsTab(context, 'Canceled'), 
-        ],
-      ),
-    );
-  }
-
-  Widget buildBookingsTab(BuildContext context, String status) {
-    return FutureBuilder<List<Map<String, dynamic>>>(
-      future: _bookingService.fetchBookings(status),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.hasError) {
-          return const Center(child: Text('Error loading bookings'));
-        }
-        if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No bookings available'));
-        }
-
-        final bookings = snapshot.data!;
-
-
-        return ListView.separated(
-          padding: const EdgeInsets.fromLTRB(12, 8, 12, 90),
-          itemCount: bookings.length,
-          separatorBuilder: (_, __) => const SizedBox(height: 6),
-          itemBuilder: (context, index) {
-            final booking = bookings[index];
-            return buildBookingCard(
-              context,
-              date: booking['date'],
-              time: booking['time'],
-              doctorName: booking['docName'],
-              specialization: booking['docSpeciality'],
-              clinic: booking['docAddress'],
-              imageUrl: booking['docImageUrl'],
-              buttons: _buildActionButtons(status),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  // Build action buttons dynamically based on status
-  Widget _buildActionButtons(String status) {
+  Widget _buildActionButtons(String status, String bookingId, Booking booking) {
     final isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
     if (status == 'Pending') {
       return Row(
         children: [
-          const Expanded(
+          Expanded(
             child: CustomButton(
               text: 'Cancel',
               color: MyColors.gray,
               textSize: 13,
-              padding: EdgeInsets.all(12),
+              padding: const EdgeInsets.all(12),
               textColor: MyColors.dark2,
+              onTap: () async {
+                await _bookingService.updateBookingStatus(
+                    bookingId, 'Canceled');
+                setState(() {});
+              },
             ),
           ),
           const SizedBox(width: 20),
@@ -133,7 +104,23 @@ class _AppointmentViewState extends State<AppointmentView>
               color: isDark ? MyColors.primary : MyColors.dark,
               textSize: 13,
               padding: const EdgeInsets.all(12),
-              textColor: isDark ? MyColors.dark: Colors.white,
+              textColor: isDark ? MyColors.dark : Colors.white,
+              onTap: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BookAppointmentView(
+                      doctorId: booking.doctorId, // Use correct field
+                      doctorName: booking.docName,
+                      doctorSpeciality: booking.docSpeciality,
+                      doctorAddress: booking.docAddress,
+                      doctorImageUrl: booking.docImageUrl,
+                      patientName: booking.patientName,
+                    ),
+                  ),
+                );
+                //
+              },
             ),
           ),
         ],
@@ -141,13 +128,17 @@ class _AppointmentViewState extends State<AppointmentView>
     } else if (status == 'Completed') {
       return Row(
         children: [
-          const Expanded(
+          Expanded(
             child: CustomButton(
               text: 'Re-Book',
               color: MyColors.gray,
               textSize: 13,
-              padding: EdgeInsets.all(12),
+              padding: const EdgeInsets.all(12),
               textColor: MyColors.dark2,
+              onTap: () async {
+                await _bookingService.updateBookingStatus(bookingId, 'Pending');
+                setState(() {});
+              },
             ),
           ),
           const SizedBox(width: 20),
@@ -157,7 +148,10 @@ class _AppointmentViewState extends State<AppointmentView>
               color: isDark ? MyColors.primary : MyColors.dark,
               textSize: 13,
               padding: const EdgeInsets.all(12),
-              textColor: isDark ? MyColors.dark: Colors.white,
+              textColor: isDark ? MyColors.dark : Colors.white,
+              onTap: () {
+                // Implement add review logic
+              },
             ),
           ),
         ],
@@ -171,7 +165,11 @@ class _AppointmentViewState extends State<AppointmentView>
               color: isDark ? MyColors.primary : MyColors.dark,
               textSize: 13,
               padding: const EdgeInsets.all(15),
-              textColor: isDark ? MyColors.dark: Colors.white,
+              textColor: isDark ? MyColors.dark : Colors.white,
+              onTap: () async {
+                await _bookingService.updateBookingStatus(bookingId, 'Pending');
+                setState(() {});
+              },
             ),
           ),
         ],
